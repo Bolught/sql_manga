b0d7e0c9f709e6ed7585c808ef0b9f0da15fb390
Bolught <vagner9823344@gmail.com>
2025-04-16 14:50:47 -0300

    classe Update_manga
    estatos: funcional e estavel

--- custom_update.py
+++ custom_update.py
@@ -1,15 +1,19 @@
 from save_book import Armazenar_livro
 from home_menu import Menu_P
+import sqlite3
 
 class Update_Manga(Armazenar_livro):  # Atualizar a lista de mangás
     def __init__(self, file_update,type_g):
       # Inicializa a classe base com conexão e cursor
         self.file_update = file_update # Nome da tabela
         self.type_g = type_g #texto do tipo de manga no capitulo_livro()
-        
+        self.manga_up = "mangas.db"
+        self.conexao_up = sqlite3.connect(self.manga_up)
+        self.cursor_up = self.conexao_up.cursor()
+
     def update(self):
         capitulo_a = Armazenar_livro(self.type_g)
-        cursor_up = Armazenar_livro(self.type_g)
+        
         while True:
             try:
                 n_manga = input("Digite o ID do mangá: ").strip()
@@ -19,13 +23,13 @@ class Update_Manga(Armazenar_livro):  # Atualizar a lista de mangás
                 n_manga = int(n_manga)
                 capitulo = capitulo_a.capitulo_livro()  # Método para obter o novo capítulo
 
-                cursor_up.cursor.execute(
+                self.cursor_up.execute(
                     f"UPDATE {self.file_update} SET capitulo = ? WHERE id = ?",
                     (capitulo, n_manga)
                 )
-                cursor_up.conexao.commit()
+                self.conexao_up.commit()
 
-                if self.cursor.rowcount == 0:
+                if self.cursor_up.rowcount == 0:
                     print("Mangá não encontrado.")
                 else:
                     print("Capítulo atualizado com sucesso.")
@@ -37,7 +41,7 @@ class Update_Manga(Armazenar_livro):  # Atualizar a lista de mangás
             finally:
                 continuar = input("Digite 'v' para voltar ao menu ou Enter para continuar: ").strip().lower()
                 if continuar == 'v':
-                    cursor_up.conexao.close()
+                    self.conexao_up.close()
                     menu = Menu_P()
                     return menu.menu()
 
